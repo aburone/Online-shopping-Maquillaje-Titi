a25039e816ebfe22e43586c086e5e50fe43a0878
Ariel Burone <correo@arielburone.com.ar>
2013-12-11 07:30:44 -0300

    FIX: bumbass

--- controllers/backend/mass_price_adjustments.rb
+++ controllers/backend/mass_price_adjustments.rb
@@ -27,8 +27,8 @@ class Backend < AppController
         DB.run 'UPDATE items
         JOIN products using(p_id)
         SET items.i_price = products.price
+        WHERE i_status IN ( "ASSIGNED", "MUST_VERIFY", "VERIFIED", "READY" )
       '
-        # WHERE i_status IN ( "ASSIGNED", "MUST_VERIFY", "VERIFIED", "READY" )
     end
     final_products
   end
