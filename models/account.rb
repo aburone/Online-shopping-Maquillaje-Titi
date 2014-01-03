959164bb6e75ca32fbff17778b455d2e2f745ff8
Ariel Burone <correo@arielburone.com.ar>
2014-01-03 02:53:42 -0300

    FEA: accounting aditional testing

--- models/account.rb
+++ models/account.rb
@@ -1,17 +1,40 @@
 # coding: utf-8
 require 'rubytree'
 
+class AccountTransaction < Sequel::Model(:accounts_transactions)
+  one_to_many :AccountRecords, key: :t_id
+  def to_s
+    out = "\n"
+    out += "#{self.class} #{sprintf("%x", self.object_id)}:\n"
+    out += "\tt_id:               #{@values[:t_id]}\n"
+    out += "\tt_desc:              #{@values[:t_desc]}\n"
+    out
+  end
+  def validate
+    super
+    validates_schema_types [:t_id, :t_id]
+    validates_schema_types [:t_desc, :t_desc]
+    errors.add(t.fields.t_desc.to_sym, t.errors.presence) if !t_desc || t_desc.empty?
+  end
+end
+
+class AccountRecord < Sequel::Model(:accounts_records)
+  many_to_one :AccountTransaction, key: :t_id
+  def validate
+    super
+    errors.add("", t.errors.record_without_transaction) if !t_id
+  end
+end
+
+
 class AccountPlan
   attr_accessor :accounts
-
   def initialize settings
     @accounts_file = File.expand_path("../../../#{settings.accounts_file}", __FILE__)
   end
-
   def save
     File.open( @accounts_file,'w') { |file| file.puts @accounts.to_yaml }
   end
-
   def load
     begin
       @accounts = YAML::load( File.read( @accounts_file ))
@@ -19,17 +42,14 @@ class AccountPlan
       @accounts = Tree::TreeNode.new("Cuentas", "Plan de cuentas")
     end
   end
-
   def account_exists? account
     return @accounts.find(account) ? true : false
   end
-
   def account_valid? account
     node = @accounts.find(account)
     return false unless node
     node.is_leaf?
   end
-
   def group account
     node = @accounts.find(account)
     while node.level > 1
@@ -40,36 +60,16 @@ class AccountPlan
 end
 
 
-
-class AccountTransaction < Sequel::Model(:accounts_transactions)
-
-  def to_s
-    out = "\n"
-    out += "#{self.class} #{sprintf("%x", self.object_id)}:\n"
-    out += "\tt_id:               #{@values[:t_id]}\n"
-    out += "\tt_desc:              #{@values[:t_desc]}\n"
-    out
-  end
-
-  def validate
-    super
-    validates_schema_types [:t_id, :t_id]
-    validates_schema_types [:t_desc, :t_desc]
-    errors.add(t.fields.t_desc.to_sym, t.errors.presence) if !t_desc || t_desc.empty?
-  end
-
-end
-
-
 class Operation
-  attr_reader :loc, :orig, :dest, :ammount, :order
-  def initialize(loc, orig, dest, ammount, order)
+  attr_reader :loc, :orig, :dest, :amount, :order
+  def initialize(loc, orig, dest, amount, order)
     @loc = loc
     @orig = orig
     @dest = dest
-    @ammount = ammount
+    @amount = amount
     @order = order
   end
+
 end
 
 
@@ -85,19 +85,23 @@ class Transaction
     @transaction.t_desc = @description
   end
 
+  def t_id
+    @transaction.t_id
+  end
+
   def to_s
     out = "\n"
-    out += "#{@description}"
+    out += "#{@description} (#{@transaction.t_id})"
     out += " $ #{l @total}" if @total != 0
     @operations.map do |o|
-      out += "\n#{o.orig} #{l o.ammount} -> #{o.dest} (#{l o.ammount}) "
+      out += "\n#{o.orig} #{l o.amount} -> #{o.dest} (#{l o.amount}) "
       out += "Orden:#{l o.order}" if o.order
       out += "@#{ConstantsTranslator.new(o.loc).t}" if o.loc
     end
     out
   end
 
-  def add(loc: nil, orig:, dest:, ammount:, order: nil)
+  def add(loc: nil, orig:, dest:, amount:, order: nil)
     raise t.errors.inexistent_account(orig) unless @plan.account_exists?(orig)
     raise t.errors.inexistent_account(dest) unless @plan.account_exists?(dest)
     raise t.errors.invalid_account(orig) unless @plan.account_valid?(orig)
@@ -105,10 +109,10 @@ class Transaction
     raise t.errors.accounts_of_same_group(orig, dest) if @plan.group(orig).name == @plan.group(dest).name
 
 
-    operation = Operation.new(loc, orig, dest, ammount, order)
+    operation = Operation.new(loc, orig, dest, amount, order)
     @operations << operation
-    @total += ammount if orig == "Caja" or orig == "Banco"
-    @total -= ammount if dest == "Caja" or dest == "Banco"
+    @total += amount if orig == "Caja" or orig == "Banco"
+    @total -= amount if dest == "Caja" or dest == "Banco"
   end
 
   def save
@@ -116,6 +120,12 @@ class Transaction
     DB.transaction do
       raise R18n.t.errors.empty_transaction if @operations.empty?
       @transaction.save
+      @operations.map do |o|
+        record = AccountRecord.new( r_orig: o.orig, r_dest: o.dest, r_amount: o.amount)
+        record.r_loc = o.loc if o.loc
+        record.o_id = o.order if o.order
+        @transaction.add_AccountRecord( record ) 
+      end
     end
   end
 
--- test/test_accounts.rb
+++ test/test_accounts.rb
@@ -17,9 +17,9 @@ class AccountsTest < Test::Unit::TestCase
 end
 
   def test_should_allow_to_add_operations
-    @transaction.add(loc: Location::S1, orig: "Recaudacion", dest: "Caja", ammount: 1000)
+    @transaction.add(loc: Location::S1, orig: "Recaudacion", dest: "Caja", amount: 1000)
     assert @transaction.has_operations? 1
-    @transaction.add(loc: Location::S1, orig: "Recaudacion", dest: "Banco", ammount: 500)
+    @transaction.add(loc: Location::S1, orig: "Recaudacion", dest: "Banco", amount: 500)
     assert @transaction.has_operations? 2
     assert_false @transaction.has_operations? 1
   end
@@ -28,7 +28,7 @@ end
   def test_transaction_description_cant_be_empty
     DB.transaction(rollback: :always) do
       transaction = Transaction.new("     " , AccountPlan.new($settings))
-      transaction.add(loc: Location::S1, orig: "Recaudacion", dest: "Caja", ammount: 1000)
+      transaction.add(loc: Location::S1, orig: "Recaudacion", dest: "Caja", amount: 1000)
       exception = assert_raise(Sequel::ValidationFailed) {transaction.save}
       assert_equal("#{t.fields.t_desc.to_sym} #{t.errors.presence}", exception.message) 
     end
@@ -36,40 +36,57 @@ end
 
 
   def test_multiple_transactions
-    transaction = Transaction.new("1 Deuda original (desglosar)" , AccountPlan.new($settings))
-    transaction.add(orig: "TODOS LOS MATERIALES", dest: "Fondo de comercio a pagar", ammount: 100000)
-    puts transaction
+    DB.transaction(rollback: :always) do
+      transaction = Transaction.new("1 Deuda original (desglosar)" , AccountPlan.new($settings))
+      transaction.add(orig: "TODOS LOS MATERIALES", dest: "Fondo de comercio a pagar", amount: 100000)
+      transaction.save
+      new_trans = AccountTransaction[transaction.t_id]
+      assert new_trans.AccountRecords.size == 1, "cantidad equivocada de registros"
+      # puts transaction
 
-    transaction = Transaction.new("2 Venta de mercaderia" , AccountPlan.new($settings))
-    transaction.add(orig: "Caja", dest: "Venta por mostrador", ammount: 10000, order: 666, loc: Location::S1)
-    transaction.add(orig: "Banco", dest: "Venta por mostrador", ammount: 5000, order: 666, loc: Location::S1)
-    transaction.add(orig: "IIBB", dest: "IIBB a pagar", ammount: 450, order: 666, loc: Location::S1)
-    transaction.add(orig: "Comisiones a pagar", dest: "Comisiones", ammount: 1500, order: 666, loc: Location::S1)
-    puts transaction
+      transaction = Transaction.new("2 Venta de mercaderia" , AccountPlan.new($settings))
+      transaction.add(orig: "Caja", dest: "Venta por mostrador", amount: 10000, order: 666, loc: Location::S1)
+      transaction.add(orig: "Banco", dest: "Venta por mostrador", amount: 5000, order: 666, loc: Location::S1)
+      transaction.add(orig: "IIBB", dest: "IIBB a pagar", amount: 450, order: 666, loc: Location::S1)
+      transaction.add(orig: "Comisiones a pagar", dest: "Comisiones", amount: 1500, order: 666, loc: Location::S1)
+      transaction.add(orig: "Costo mercaderia vendida", dest: "TODOS LOS PRODUCTOS", amount: 10000, order: 666, loc: Location::S1)
+      transaction.save
+      new_trans = AccountTransaction[transaction.t_id]
+      assert new_trans.AccountRecords.size == 5, "cantidad equivocada de registros"
+      # puts transaction
 
-    transaction = Transaction.new("2a costo de mercaderia (desglosar)" , AccountPlan.new($settings))
-    transaction.add(orig: "Costo mercaderia vendida", dest: "TODOS LOS PRODUCTOS", ammount: 10000, order: 666, loc: Location::S1)
-    puts transaction
+      transaction = Transaction.new("2b Cobro de comisiones" , AccountPlan.new($settings))
+      transaction.add(orig: "Comisiones", dest: "Caja", amount: 1500, loc: Location::S1)
+      transaction.save
+      new_trans = AccountTransaction[transaction.t_id]
+      assert new_trans.AccountRecords.size == 1, "cantidad equivocada de registros"
+      # puts transaction
 
-    transaction = Transaction.new("2b Cobro de comisiones" , AccountPlan.new($settings))
-    transaction.add(orig: "Comisiones", dest: "Caja", ammount: 1500, loc: Location::S1)
-    puts transaction
+      transaction = Transaction.new("3 Compra de impresora" , AccountPlan.new($settings))
+      transaction.add(orig: "Bienes de uso", dest: "Tarjeta de credito a pagar", amount: 1000, loc: Location::W2)
+      transaction.save
+      new_trans = AccountTransaction[transaction.t_id]
+      assert new_trans.AccountRecords.size == 1, "cantidad equivocada de registros"
+      # puts transaction
 
-    transaction = Transaction.new("3 Compra de impresora" , AccountPlan.new($settings))
-    transaction.add(orig: "Bienes de uso", dest: "Tarjeta de credito a pagar", ammount: 1000, loc: Location::W2)
-    puts transaction
+      transaction = Transaction.new("4 Compra de mercaderia" , AccountPlan.new($settings))
+      transaction.add(orig: "Liquido corporal blanco", dest: "Laca", amount: 230)
+      transaction.add(orig: "Liquido corporal rojo", dest: "Laca", amount: 250)
+      transaction.add(orig: "Laca", dest: "Caja", amount: 480)
+      transaction.add(orig: "Fletes", dest: "Caja", amount: 20)
+      transaction.save
+      new_trans = AccountTransaction[transaction.t_id]
+      assert new_trans.AccountRecords.size == 4, "cantidad equivocada de registros"
+      # puts transaction
 
-    transaction = Transaction.new("4 Compra de mercaderia" , AccountPlan.new($settings))
-    transaction.add(orig: "Liquido corporal blanco", dest: "Laca", ammount: 230)
-    transaction.add(orig: "Liquido corporal rojo", dest: "Laca", ammount: 250)
-    transaction.add(orig: "Laca", dest: "Caja", ammount: 480)
-    transaction.add(orig: "Fletes", dest: "Caja", ammount: 20)
-    puts transaction
-
-    transaction = Transaction.new("5 Aumento del 10% de la mercaderia recien comprada" , AccountPlan.new($settings))
-    transaction.add(orig: "Liquido corporal blanco", dest: "Resultado por tenencia", ammount: 23)
-    transaction.add(orig: "Liquido corporal rojo", dest: "Resultado por tenencia", ammount: 25)
-    puts transaction
+      transaction = Transaction.new("5 Aumento del 10% de la mercaderia recien comprada" , AccountPlan.new($settings))
+      transaction.add(orig: "Liquido corporal blanco", dest: "Resultado por tenencia", amount: 23)
+      transaction.add(orig: "Liquido corporal rojo", dest: "Resultado por tenencia", amount: 25)
+      transaction.save
+      new_trans = AccountTransaction[transaction.t_id]
+      assert new_trans.AccountRecords.size == 2, "cantidad equivocada de registros"
+      # puts transaction
+    end
   end
 
   def test_AccountPlan_should_detect_invalid_accounts
@@ -88,7 +105,34 @@ end
     transaction = Transaction.new("Compra de liquido corporal mal registrada", account_plan)
     orig = "Liquido corporal blanco"
     dest = "Caja"
-    exception = assert_raise(RuntimeError) {transaction.add(orig: orig, dest: dest, ammount: 500)}
+    exception = assert_raise(RuntimeError) {transaction.add(orig: orig, dest: dest, amount: 500)}
     assert_equal(R18n.t.errors.accounts_of_same_group(orig, dest), exception.message) 
   end
+
+  def test_operation_shoud_have_a_transaction
+    DB.transaction(rollback: :always) do
+      record = AccountRecord.new( r_orig: "Viaticos", r_dest: "Caja", r_amount: "10")
+      exception = assert_raise(Sequel::ValidationFailed) { record.save }
+      assert_equal(" #{R18n.t.errors.record_without_transaction}", exception.message) 
+    end
+  end
+
+  def test_operation_shoud_have_origin_destination_and_amount
+    account_plan = AccountPlan.new($settings)
+    transaction = Transaction.new("Registro de viaticos sin destino", account_plan)
+    orig = "Viaticos"
+    dest = "Caja"
+    amount = 500
+
+    exception = assert_raise(ArgumentError) {transaction.add(dest: dest, amount: amount)}
+    assert_equal("missing keyword: orig", exception.message) 
+
+    exception = assert_raise(ArgumentError) {transaction.add(orig: orig, amount: amount)}
+    assert_equal("missing keyword: dest", exception.message) 
+
+    exception = assert_raise(ArgumentError) {transaction.add(orig: orig, dest: dest)}
+    assert_equal("missing keyword: amount", exception.message) 
+  end
+
+
 end
