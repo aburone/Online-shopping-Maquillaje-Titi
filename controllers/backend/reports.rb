766612283291bbce4fb4a8a647058d9b6792140d
Ariel Burone <correo@arielburone.com.ar>
2014-02-10 06:24:21 -0300

    FEA: products to buy report

--- controllers/backend/reports.rb
+++ controllers/backend/reports.rb
@@ -8,7 +8,22 @@ class Backend < AppController
       price_pro_col: false,
       stock_col: false,
       real_markup_col: true,
-      markup_deviation_col: true
+      markup_deviation_col: true,
+      persistent_headers: true
+    }
+  end
+
+  get '/reports/to_buy' do
+    list = Product.new.get_list.where(tercerized: true)
+    @products = Product.new.get_saleable_at_all_locations list
+    slim :products_list, layout: :layout_backend, locals: {title: "Reporte de productos por comprar", sec_nav: :nav_administration,
+      can_edit: true, edit_link: :edit_product,
+      full_row: true,
+      price_pro_col: false,
+      stock_col: false,
+      multi_stock_col: true,
+      persistent_headers: true
+
     }
   end
 
