8bd3554f2a3af2a8d68c9fe7fcaed0394b2ae600
Ariel Burone <correo@arielburone.com.ar>
2014-02-10 07:44:34 -0300

    FIX: better headers

--- controllers/backend/reports.rb
+++ controllers/backend/reports.rb
@@ -16,7 +16,7 @@ class Backend < AppController
   get '/reports/to_buy' do
     list = Product.new.get_list.where(tercerized: true)
     @products = Product.new.get_saleable_at_all_locations list
-    slim :products_list, layout: :layout_backend, locals: {title: "Reporte de productos por comprar", sec_nav: :nav_administration,
+    slim :products_list, layout: :layout_backend, locals: {title: "Reporte de productos por comprar (no terminado)", sec_nav: :nav_administration,
       can_edit: true, edit_link: :edit_product,
       full_row: true,
       price_pro_col: false,
