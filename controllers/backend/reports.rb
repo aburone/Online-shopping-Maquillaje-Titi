60dbf4104f4e1a06cdb9a03e379caf7b587dd821
Ariel Burone <correo@arielburone.com.ar>
2014-02-10 09:07:53 -0300

    FEA: buy and send reports

--- controllers/backend/reports.rb
+++ controllers/backend/reports.rb
@@ -21,22 +21,45 @@ class Backend < AppController
       full_row: true,
       price_pro_col: false,
       stock_col: false,
+      persistent_headers: true,
       multi_stock_col: true,
-      persistent_headers: true
-
+      stock_deviation_col: true
     }
   end
 
   get '/reports/stocks' do
     list = Product.new.get_list.where(tercerized: false)
     @products = Product.new.get_saleable_at_all_locations list
-    slim :products_list, layout: :layout_backend, locals: {title: "Reporte de existencias", sec_nav: :nav_production,
-      can_edit: true, edit_link: :edit_product,
+    slim :products_list, layout: :layout_backend, locals: {title: "Reporte de productos por envasar", sec_nav: :nav_production,
+      can_edit: false,
       full_row: true,
       price_pro_col: false,
       stock_col: false,
       multi_stock_col: true,
+      stock_deviation_col: true,
       persistent_headers: true
+    }
+  end
+
+  get '/reports/to_move' do
+    list = Product.new.get_list
+    products = Product.new.get_saleable_at_all_locations list
+    stock_location_name = "stock_#{current_location[:name].downcase}".to_sym
+    @products = []
+    products.map do |product| 
+      product[:to_move] = 0
+      product[:to_move] = product[:ideal_stock] - product[:stock_store_1] unless product[:stock_store_1] > product[:ideal_stock]
+      product[:to_move] = product[stock_location_name] if product[:to_move] >= product[stock_location_name]
+      @products << product
+    end
+
+    slim :products_list, layout: :layout_backend, locals: {title: "Reporte de productos por enviar al local 1 (no terminado)", sec_nav: :nav_production,
+      can_edit: false,
+      full_row: true,
+      price_pro_col: false,
+      persistent_headers: true,
+      multi_stock_col: true,
+      to_move_col: true
     }
   end
 
