4d0901645c9c216579808cc737eb1eef587dcbb0
Ariel Burone <correo@arielburone.com.ar>
2014-05-18 04:06:58 -0300

    FEA: better aside menu

--- models/distributor.rb
+++ models/distributor.rb
@@ -3,7 +3,11 @@ require 'sequel'
 
 class Distributor < Sequel::Model(:distributors)
   many_to_many :products , left_key: :d_id, right_key: :p_id, join_table: :products_to_distributors
+  many_to_many :materials , left_key: :d_id, right_key: :m_id, join_table: :materials_to_distributors
 end
 
 class ProductDistributor < Sequel::Model(:products_to_distributors)
+end
+
+class MaterialDistributor < Sequel::Model(:materialss_to_distributors)
 end
