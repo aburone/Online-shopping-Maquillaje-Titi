e957247b361ab38ad84e961c188eb2932e802f52
Ariel Burone <correo@arielburone.com.ar>
2014-07-23 06:33:56 -0300

    FEA: sales orders revision, show if the sale was regular or profesional issue 75500572

--- controllers/shared/orders.rb
+++ controllers/shared/orders.rb
@@ -22,9 +22,20 @@ module Orders
         @orders = Order.new.get_orders_at_location_with_type(params[:location], params[:type]).order(:o_id).reverse.limit(500).all
       elsif params[:o_id].to_i > 0
         @order = Order.new.get_orders_at_location_with_type_and_id(params[:location], params[:type], params[:o_id])
-        @cart_total = @order.cart_total if @order.type == Order::SALE
         @items = @order.items
         @bulks = @order.bulks
+        if @order.type == Order::SALE
+          payments_total = 0 # use Order.payments_total
+          BookRecord.where(o_id: @order.o_id).all.each { |payment| payments_total += payment.amount }
+
+          reg_sum = 0
+          pro_sum = 0
+          @items.each do |item|
+            reg_sum =+ item.i_price
+            pro_sum =+ item.i_price_pro
+          end
+          payment_type = payments_total == reg_sum ? "normal" : "profesional"
+        end
       end
     end
 
@@ -32,7 +43,8 @@ module Orders
       credits = @order.credits
       slim :credit_note, layout: Thread.current.thread_variable_get(:layout), locals: {order: @order, credits: credits, sec_nav: :nav_orders, base_url: request.env['REQUEST_PATH']}
     else
-      slim :orders, layout: Thread.current.thread_variable_get(:layout), locals: {sec_nav: :nav_orders, base_url: request.env['REQUEST_PATH'], title: t.orders.title}
+      slim :orders, layout: Thread.current.thread_variable_get(:layout),
+        locals: {sec_nav: :nav_orders, base_url: request.env['REQUEST_PATH'], title: t.orders.title, payments_total: payments_total, payment_type: payment_type}
     end
   end
 end
