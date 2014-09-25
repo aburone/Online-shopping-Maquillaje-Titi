6e469abfba1bef79b68bb6685f5c631bd07ee80b
Ariel Burone <correo@arielburone.com.ar>
2014-09-25 05:03:07 -0300

    frontend v1

--- frontend.rb
+++ frontend.rb
@@ -10,7 +10,7 @@ class Frontend < AppController
     etag settings.start_time.to_s
     cache_control :public, :must_revalidate
   end
-    
+
   get '/' do
     slim :home, layout: :layout_frontend
   end
@@ -20,17 +20,20 @@ class Frontend < AppController
   end
 
   get '/fotos/' do
-    slim :fotos, layout: :layout_frontend
+    slim :fotos, layout: :layout_frontend, locals: {gallery: Gallery.all.reverse}
   end
 
   get '/productos/' do
-    @categories = Category.all
-    slim :categorias, layout: :layout_frontend
+    slim :frontend_categories, layout: :layout_frontend, locals: {categories: Category.all}
+  end
+
+  get '/productos/cat/:cat_id' do
+    slim :frontend_category, layout: :layout_frontend, locals: {category: Category[params[:cat_id]], products: Product.new.get_live.where(c_id: params[:cat_id]).all}
   end
 
   get '/productos/:id' do
     @category = Category[params[:id].to_i]
-    slim :categoria, layout: :layout_frontend
+    slim :category, layout: :layout_frontend
   end
 
 end
